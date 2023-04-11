5592b62b705fdfd9650d8ff27b3eae42108f20d1
Mari-27 <zuzikovamaria25@gmail.com>
2023-04-11 19:31:05 +0500

    Небольшие изменения

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -3,27 +3,31 @@ function Student(name, gender, age) {
     this.gender = gender;
     this.age = age;
     this.marks = [];
-};
+  }
   
-  Student.prototype.setSubject = function (subjectName) {
+  Student.prototype.setSubject = function(subjectName) {
     this.subject = subjectName;
   }
   
-  Student.prototype.addMarks = function (...marks) {
-    if ('marks' in this === true) {
-        this.marks.push(...marks)
+  Student.prototype.addMarks = function(...marksToAdd) {
+    if (!this.marks) {
+      console.log("Студент исключен из учебного процесса");
+      return;
     }
-};
-
-Student.prototype.getAverage = function () {
-    if ('marks' in this === false || this.marks.length === 0) {
-        return 0
-    } 
-    return this.marks.reduce((acc, currentEstimate) => acc + currentEstimate, 0) / this.marks.length;
-};
-
-Student.prototype.exclude = function (reason) {
+    this.marks.push(...marksToAdd);
+  }
+  
+  Student.prototype.getAverage = function()  {
+     if ('marks' in this === false || this.marks.length === 0) {
+    return 0
+} 
+return this.marks.reduce((acc, currentEstimate) => acc + currentEstimate, 0) / this.marks.length;
+  
+  }
+ 
+  Student.prototype.exclude = function(reason) {
     delete this.subject;
     delete this.marks;
     this.excluded = reason;
-};
+  }
+  
